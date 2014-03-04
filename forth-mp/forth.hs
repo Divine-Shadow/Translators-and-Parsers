9be6614f34645d9fc1606bbb0d01205b2f77d63e
th3 d1v1n3 5h4d0w <wmolchan@hawk.iit.edu>
2014-03-04 17:12:34 -0600

    made tokenizer functional

--- forth-mp/forth.hs
+++ forth-mp/forth.hs
@@ -46,8 +46,10 @@ dinsert key val dict =
    case H.lookup key dict of
       Nothing -> H.insert key [val] dict
       Just x  -> H.insert key (val:x) dict
- -- my functions     
-   -- my manipulators
+ 
+-- My functions part I   
+      
+   -- My stack manipulators
 drop2 (x:xs) = xs
 drop2 _ = []
 
@@ -62,36 +64,23 @@ swap _ = eUnderflow
 rot (x:f:d:xs) = (d:x:f:xs)
 rot _ = eUnderflow
 
-  -- my own if statement
-ifff a t f  
-  | a==True = t
-  | otherwise = f
-                
-
   -- my comparison operators
 lessThan (a:b:c) = (ifff (b < a) (-1) 0):c
 greaterThan (a:b:c) = (ifff (b > a) (-1) 0):c
 equalto  (a:b:c) = (ifff (a == b) (-1) 0):c
 
-
+  -- my own if statement
+ifff a t f  
+  | a==True = t
+  | otherwise = f
 
 -- Initial Dictionary
 
-{-dictionary0 = dinsert "+" (Prim $ wrap2 (+)) H.empty
-dictionary1 = dinsert "drop" (Prim $ drop2) dictionary0
-dictionary2 = dinsert "rot" (Prim $ rot ) dictionary1
-dictionary3= dinsert "swap" (Prim $ swap ) dictionary2
-dictionary4= dinsert "dup" (Prim $ dup ) dictionary3
-dictionary5 = dinsert "-" (Prim $ wrap2 (-)) dictionary4
-dictionary6 = dinsert "<" (Prim $ lessThan) dictionary5
-dictionary7 = dinsert ">" (Prim $ greaterThan) dictionary6
-dictionary8 = dinsert "=" (Prim $ equalto) dictionary7
-dictionary9 = dinsert "*" (Prim $ wrap2 (*)) dictionary8
-dictionary  = dinsert "/" (Prim $ wrap2 div) dictionary9 -}
- -- A more functional approach to initializing the values
+  -- A more functional approach to initializing the values
 initDict (q:qs) (a:as) dict =  dinsert q (Prim $ a) (initDict qs as dict) 
 initDict [] _ dict = dict
 
+  -- And said initialization
 dictionary = initDict [ "+", "drop", "rot", "swap" , "dup" , "-",  "<",  ">",  "=" ,  "*", "/"] [ wrap2 (+), drop2, rot, swap, dup, wrap2 (-), lessThan, greaterThan, equalto, wrap2 (*), wrap2 div] H.empty 
 
 -- The Evaluator
@@ -99,6 +88,7 @@ dictionary = initDict [ "+", "drop", "rot", "swap" , "dup" , "-",  "<",  ">",  "
 eval :: [String] -> ForthState -> IO ForthState
 eval []    (istack, [],     dict) = return (istack, [], dict)
 eval [] (istack, (h:hs) , dict) = eval h (istack, hs , dict)
+
 eval words (istack, cstack, dict) =
   case dlookup (head words) dict of
     Num i        -> eval xs (i:istack, cstack, dict)
@@ -109,10 +99,10 @@ eval words (istack, cstack, dict) =
                              eval xs (istack, cstack, dict)}
     Unknown ":"  -> eval (drop (findSplit xs 1) xs) (istack, cstack, dinsert (head xs) (Def (tail (take (findSplit xs 0) xs))) dict)
     Unknown "if" -> eval whichBranch ((tail istack), remainingWords:cstack, dict)
-                    
+    Unknown "then" -> eval (head cstack) (istack, (tail cstack), dict)                
     Unknown "begin" -> eval loopContents (istack, loopContents:((drop againloc xs):cstack), dict) 
     Unknown "again" -> eval (head cstack) (istack, cstack, dict)
-    Unknown "exit" -> eval (head (tail cstack)) (istack, (tail (tail cstack)), dict)
+    Unknown "exit" -> eval (head ( tail (tail cstack))) (istack, (tail (tail cstack)), dict)
     
     Def p  ->  eval  p (istack, xs:cstack, dict)
   
@@ -122,41 +112,44 @@ eval words (istack, cstack, dict) =
         loopContents = take againloc xs
         againloc = whereAgain xs 1
 
--- finds the else token, if a is 0 then the take value of the output will exclude the else statement, if a is 1 the drop value of the return will drop the else statement
-whereElse (d:ds) a
-  | d == "else" = a
-  | otherwise = whereElse ds (a+1)
-whereElse [] _ = -1
 
-whereAgain (d:ds) a
-  | d == "again" = a
-  | otherwise = whereAgain ds (a+1)
-whereAgain [] _ = -1
+-- My functions part II
+        
+ -- Tokens
 
--- finds the then token, if a is 0 then the take value of the output will exclude the then statement, if a is 1 the drop value of the return will drop the Then statement
-whereThen (d:ds) a
-  | d == "then" = a
-  | otherwise = whereThen ds (a+1)
-                
--- returns the true branch of an expression
+  -- finds the  token 
+  -- if a is 0 then the "take" value of the output will exclude the token
+  -- if a is 1 the "drop" value of the return will drop the token statement
+tokenFinder (d:ds) a token
+  | d == token = a
+  | otherwise = tokenFinder ds (a+1) token
+tokenFinder [] _  _ = -1
+
+  -- frequently used tokens
+whereAgain m a = tokenFinder m a "again"
+whereThen m a = tokenFinder m a "then"
+whereElse m a = tokenFinder m a "else"
+findSplit m a = tokenFinder m a ";"
+
+ -- True False path identifiers
+  -- returns the true branch of an expression
 trueBranch ds
- | whereElse ds 0== -1 = take (whereThen ds 0) ds
+ | whereElse ds 0== -1 = take (whereThen ds 1) ds
  | otherwise = take (whereElse ds 0) ds
                
--- retruns the false branch if it exists and nil if it doesn't
+  -- retruns the false branch if it exists and nil if it doesn't
 falseBranch ds
   | whereElse ds 0== -1 = []
-  | otherwise = take (whereThen newString 0) newString
+  | otherwise = take (whereThen newString 1) newString
   where newString = drop (whereElse ds 1) ds                     
                 
-                
-findSplit (c:cs) a
-  | c == ";" = a
-  | otherwise = findSplit cs (a + 1)
-
+ 
+-- prints the stack, used in .S
 printstack (b:bs) = printstack bs ++ show b ++ " "
 printstack _ = ""
 
+
+-- The repl
 repl :: ForthState -> IO ForthState
 repl state =
   do putStr "> " ;
